a37af8ec6865e9a9cfd2f1a30b9e778cb4f08bd2
adriane <adrianedacosta21@gmail.com>
2022-04-25 17:41:53 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int primeiro, segundo, maiorNumero;
+
+Console.Write("Digite o primeiro numero...:");
+primeiro = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo numero...:");
+segundo = Convert.ToInt32(Console.ReadLine());
+
+if (primeiro < segundo)
+{
+    maiorNumero= segundo;
+}
+else
+{
+    maiorNumero = primeiro;
+}
+
+Console.WriteLine($"o maior numero é {maiorNumero}");
